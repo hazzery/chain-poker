92fcf3868c573972b94eb2add936e018ecbcae39
Harrison Parkes <hpa101@uclive.ac.nz>
2025-05-16 13:03:58 +1200

    Feature: Created data structures to hold basic game data

--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,46 @@
 use schemars::JsonSchema;
+use secret_toolkit::storage::Item;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::{Addr, Storage};
 use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
+
+pub static GAME: Item<Game> = Item::new(b"game");
+
+pub enum Suit {
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades,
+}
+
+pub enum Value {
+    Ace,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+}
+
+pub struct Card {
+    pub suit: Suit,
+    pub value: Value,
+}
+
+pub struct Player {
+    pub hand: Option<(Card, Card)>,
+}
+
+pub struct Game {
+    pub table: Vec<Card>,
+    pub players: Vec<Player>,
+}
