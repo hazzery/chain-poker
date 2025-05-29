fb1bdc143a1a6ec6b7630f539b832e720e7680fb
Harrison Parkes <hpa101@uclive.ac.nz>
2025-05-29 17:46:03 +1200

    Fix: committed new files missing from previous commit

---
+++ frontend/src/pages/Game.tsx
@@ -0,0 +1,53 @@
+import { Box } from "@mui/material";
+
+import { ChipCount } from "../ChipCount";
+import { CardSet } from "../components/CardSet";
+import FanLayout from "../components/FanLayout";
+import { Hand } from "../components/Hand";
+import NavBar from "../components/NavBar";
+import { Rank, Suit } from "../components/PlayingCard";
+
+function Game() {
+  const playersHand = [
+    { suit: Suit.Hearts, rank: Rank.Ace },
+    { suit: Suit.Diamonds, rank: Rank.Ace },
+  ];
+
+  const table = [
+    { suit: Suit.Clubs, rank: Rank.Ace },
+    { suit: Suit.Clubs, rank: Rank.Seven },
+    { suit: Suit.Diamonds, rank: Rank.Two },
+    { suit: Suit.Hearts, rank: Rank.Two },
+    { suit: Suit.Spades, rank: Rank.Four },
+  ];
+
+  const players = [
+    { name: "John", chipBalance: 70 },
+    { name: "James", chipBalance: 80 },
+    { name: "Jimmy", chipBalance: 90 },
+    { name: "Jeff", chipBalance: 60 },
+    { name: "Jake", chipBalance: 50 },
+    { name: "Jacob", chipBalance: 110 },
+    { name: "Jack", chipBalance: 120 },
+    { name: "Jamie", chipBalance: 70 },
+  ];
+
+  const currentPot = 10;
+  return (
+    <Box display="flex" flexDirection="column" height="100vh">
+      <NavBar />
+      <FanLayout players={players}>
+        <CardSet cards={table} />
+        <ChipCount
+          numberOfChips={currentPot}
+          chipIconSize="3em"
+          fontSize="2em"
+          sx={{ justifyContent: "center", display: "flex" }}
+        />
+      </FanLayout>
+      <Hand cards={playersHand} chipBalance={97.5} />
+    </Box>
+  );
+}
+
+export default Game;
---
+++ frontend/src/pages/Landing.tsx
@@ -0,0 +1,122 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { GiPokerHand } from "react-icons/gi";
+
+import contractExecute from "../../../node/src/execute.ts";
+import NavBar from "../components/NavBar";
+import { useState } from "preact/hooks";
+import type { ReactNode } from "preact/compat";
+import contractCodeHash from "../../resources/codeHash.json";
+
+const enum LandingMode {
+  Main,
+  Create,
+  Join,
+}
+
+function Landing() {
+  const [mode, setMode] = useState(LandingMode.Main);
+
+  function goBack(): void {
+    setMode(LandingMode.Main);
+  }
+
+  function joinLobby(contractAddress: string): void {
+    contractExecute(
+      {}, // TODO: Specify buy in ammount
+      10000,
+      { contractAddress, contractCodeHash },
+      wallet,
+      networkClient,
+    );
+  }
+
+  function showContent(): ReactNode {
+    switch (mode) {
+      case LandingMode.Main:
+        return (
+          <>
+            <Button
+              variant="outlined"
+              color="success"
+              onClick={() => setMode(LandingMode.Create)}
+            >
+              Create lobby
+            </Button>
+            <Button
+              variant="outlined"
+              color="success"
+              onClick={() => setMode(LandingMode.Join)}
+            >
+              Join existing loby
+            </Button>
+          </>
+        );
+      case LandingMode.Create:
+        return (
+          <>
+            <Box display="flex" columnGap="1em">
+              <TextField
+                variant="outlined"
+                label={"Enter contract code ID"}
+              ></TextField>
+              <Button variant="outlined" color="success">
+                Create
+              </Button>
+            </Box>
+            <Button
+              variant="outlined"
+              color="inherit"
+              sx={{ width: "6em" }}
+              onClick={goBack}
+            >
+              Back
+            </Button>
+          </>
+        );
+      case LandingMode.Join:
+        return (
+          <>
+            <Box display="flex" columnGap="1em">
+              <TextField variant="outlined" label={"Enter contract address"}>
+                Hello
+              </TextField>
+              <Button variant="outlined" color="success">
+                Join
+              </Button>
+            </Box>
+            <Button
+              variant="outlined"
+              color="inherit"
+              onClick={goBack}
+              sx={{ width: "6em" }}
+            >
+              Back
+            </Button>
+          </>
+        );
+    }
+  }
+
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <NavBar />
+      <Box display="flex" alignItems="center" marginTop="5em">
+        <GiPokerHand size="20em" />
+        <Typography fontSize="5em">Chain Poker</Typography>
+      </Box>
+      <Box
+        sx={{
+          padding: "2em",
+          display: "flex",
+          flexDirection: "column",
+          columnGap: "1em",
+          rowGap: "1em",
+        }}
+      >
+        {showContent()}
+      </Box>
+    </Box>
+  );
+}
+
+export default Landing;
