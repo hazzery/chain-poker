b8b774334ef01e33a4d985d01d75f6499b5fcea8
Harrison Parkes <hpa101@uclive.ac.nz>
2025-05-30 17:03:09 +1200

    Documentation: Added missing documentation from previous commit

--- node/src/upload.ts
+++ node/src/upload.ts
@@ -9,6 +9,16 @@ import { Result } from "typescript-result";
 import Err from "./err";
 import { UploadData } from "./types";
 
+/**
+ * Process the respopnse from the contract, returning error if correct
+ * information cannot be found from the transaction response.
+ *
+ * @param transaction - The transaction to process.
+ * @param networkClient - A Secret Network client.
+ *
+ * @returns A result containing an object with the code ID and the contract
+ *    code hash if sucessfull, otherwise an error.
+ */
 async function processUploadTransactionResponse(
   transaction: TxResponse,
   networkClient: SecretNetworkClient,
@@ -54,7 +64,7 @@ async function processUploadTransactionResponse(
  * @param wallet - A wallet initialised with a private key.
  * @param networkClient - A Secret Network client, initialised with `wallet`.
  * @param contractWasm - A buffer containing the contract's compiled binary Web
- *    Assembly code
+ *    Assembly code.
  *
  * @returns A result containing an object with the code ID and the contract
  *    code hash if sucessfull, otherwise an error.
