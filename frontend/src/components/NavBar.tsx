6523ad49a2daabddc5a3f9c0fcfc0f1b6dbaa986
Harrison Parkes <hpa101@uclive.ac.nz>
2025-06-01 13:53:16 +1200

    Feature: Added icon to navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,12 +1,14 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { GiPokerHand } from "react-icons/gi";
 
 function NavBar() {
   return (
     <AppBar color="success">
       <Toolbar>
-        <Typography variant="h6" component="div">
+        <GiPokerHand size="2.5em"/>
+        <Typography variant="h6" component="div" marginLeft="0.7em">
           Chain Poker
         </Typography>
       </Toolbar>
