75c2c50818e38f241cd071e7424ee5c0f76897f1
Harrison Parkes <hpa101@uclive.ac.nz>
2025-06-05 10:02:37 +1200

    Feature: CreateLobby no longer redirects to BuyIn, a new buy in box appears after lobby is created

--- frontend/src/components/CreateLobby.tsx
+++ frontend/src/components/CreateLobby.tsx
@@ -1,9 +1,11 @@
-import { Box, Button } from "@mui/material";
-import type { JSX, VNode } from "preact";
+import { Box, Button, Divider } from "@mui/material";
+import type { VNode } from "preact";
 import { useLocation } from "preact-iso";
 import type { SecretNetworkClient } from "secretjs";
+
+import { useState } from "preact/hooks";
 import useNumberValidation from "../hooks/useNumberValidation";
-import { createLobby } from "../secretnetwork/chainPokerContract";
+import { buyIn, createLobby } from "../secretnetwork/chainPokerContract";
 import TextInput from "./TextInput";
 
 interface CreateLobbyProps {
@@ -13,22 +15,23 @@ interface CreateLobbyProps {
 
 function CreateLobby({ backAction, networkClient }: CreateLobbyProps): VNode {
   const location = useLocation();
-  const [bigBlind, setBigBlind] = useNumberValidation({
-    integer: true,
+  const [bigBlind, setBigBlind] = useNumberValidation({ required: true });
+  const [minBuyInBB, setMinBuyInBB] = useNumberValidation({ required: true });
+  const [maxBuyInBB, setMaxBuyInBB] = useNumberValidation({ required: true });
+  const [lobbyCode, setLobbyCode] = useState<string | null>(null);
+  const [buyInAmount, setBuyInAmount] = useNumberValidation({
     required: true,
-  });
-  const [minBuyInBB, setMinBuyInBB] = useNumberValidation({
-    integer: true,
-    required: true,
-  });
-  const [maxBuyInBB, setMaxBuyInBB] = useNumberValidation({
-    integer: true,
-    required: true,
+    maxValue: Number(maxBuyInBB.value) * Number(bigBlind.value),
+    minValue: Number(minBuyInBB.value) * Number(bigBlind.value),
   });
 
-  async function handleSubmit(event: JSX.TargetedSubmitEvent<HTMLFormElement>) {
-    event.preventDefault();
+  async function handleBuyIn() {
+    await buyIn(Number(buyInAmount.value), lobbyCode!, networkClient)
+      .onSuccess(() => location.route(`/play/${lobbyCode}`))
+      .onFailure(console.dir);
+  }
 
+  async function handleCreateLobby() {
     await createLobby(
       {
         big_blind: Number(bigBlind.value),
@@ -37,25 +40,19 @@ function CreateLobby({ backAction, networkClient }: CreateLobbyProps): VNode {
       },
       networkClient,
     )
-      .onSuccess((lobbyCode) => location.route(`/play/${lobbyCode}/buy-in`))
+      .onSuccess(setLobbyCode)
       .onFailure(console.dir);
   }
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      display="flex"
-      flexDirection="column"
-      rowGap="1em"
-      width="20em"
-    >
+    <Box display="flex" flexDirection="column" rowGap="1em" width="20em">
       <TextInput
         required
         fullWidth
         state={bigBlind}
         setState={setBigBlind}
         label="Big blind value (SCRT)"
+        disabled={lobbyCode !== null}
         variant="outlined"
         color="success"
       />
@@ -65,6 +62,7 @@ function CreateLobby({ backAction, networkClient }: CreateLobbyProps): VNode {
         state={maxBuyInBB}
         setState={setMaxBuyInBB}
         label="Maximum buy in (number of big blinds)"
+        disabled={lobbyCode !== null}
         variant="outlined"
         color="success"
       />
@@ -74,6 +72,7 @@ function CreateLobby({ backAction, networkClient }: CreateLobbyProps): VNode {
         state={minBuyInBB}
         setState={setMinBuyInBB}
         label="Minimum buy in (number of big blinds)"
+        disabled={lobbyCode !== null}
         variant="outlined"
         color="success"
       />
@@ -82,13 +81,34 @@ function CreateLobby({ backAction, networkClient }: CreateLobbyProps): VNode {
         disabled={
           bigBlind.error !== null ||
           minBuyInBB.error !== null ||
-          maxBuyInBB.error !== null
+          maxBuyInBB.error !== null ||
+          lobbyCode !== null
         }
-        type="submit"
+        onClick={handleCreateLobby}
         variant="outlined"
         color="success"
       >
         Create
+      </Button>
+      <Divider sx={{ display: lobbyCode !== null ? "inline" : "none" }} />
+      <TextInput
+        required
+        fullWidth
+        state={buyInAmount}
+        setState={setBuyInAmount}
+        label="Buy in amount"
+        color="success"
+        sx={{ display: lobbyCode !== null ? "inline" : "none" }}
+      />
+      <Button
+        fullWidth
+        disabled={buyInAmount.error !== null}
+        onClick={handleBuyIn}
+        variant="outlined"
+        color="success"
+        sx={{ display: lobbyCode !== null ? "inline" : "none" }}
+      >
+        Buy in
       </Button>
       <Button
         onClick={backAction}
