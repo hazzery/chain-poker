b9537559d4decaf0e85b1ad2fa826bb0a87d375f
Harrison Parkes <hpa101@uclive.ac.nz>
2025-06-15 09:22:34 +1200

    Fix: Player balances now correctly update each re-render

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,11 +1,10 @@
 import { Box } from "@mui/material";
 import type { VNode } from "preact";
-import { useState } from "preact/hooks";
 import type { SecretNetworkClient } from "secretjs";
 import { Result } from "typescript-result";
 
 import { placeBet } from "../secretnetwork/chainPokerContract";
-import type { GameState, PlayerInfo } from "../secretnetwork/types";
+import type { GameState } from "../secretnetwork/types";
 import CardSet from "./CardSet";
 import { ChipCount } from "./ChipCount";
 import FanLayout from "./FanLayout";
@@ -27,12 +26,13 @@ function Game({
   lobbyCode,
 }: GameProps): VNode | undefined {
   const playersUsername = localStorage.getItem("username");
-  const [playerInfos] = useState<PlayerInfo[]>(
-    balances.map(([name, chipBalance]) => ({ name, chipBalance })),
-  );
-  const [chipBalance] = useState(
-    balances.find(([username]) => username === playersUsername)![1],
-  );
+  const playerInfos = balances.map(([name, chipBalance]) => ({
+    name,
+    chipBalance,
+  }));
+  const chipBalance = balances.find(
+    ([username]) => username === playersUsername,
+  )![1];
 
   async function sendBet(betAmount: number): Promise<void> {
     await Result.fromAsync(
